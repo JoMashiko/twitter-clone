bfbe38246e6e7122259aa02aa12823a38d098f3b
JoMashiko <j.mashiko@hajimari.inc>
2023-09-12 14:23:38 +0900

    aタグをButtonに変更

--- twitter/resources/views/layouts/app.blade.php
+++ twitter/resources/views/layouts/app.blade.php
@@ -55,15 +55,13 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('user.show', Auth::id()) }}">
-                                     {{ __('マイページ') }}
-                                    </a>
+                                    <button type="button" class="dropdown-item" onclick="location.href='{{ route('user.show', Auth::id()) }}'">
+                                        {{ __('マイページ') }}
+                                    </button>
 
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+                                    <button type="button" class="dropdown-item" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                        {{ __('ログアウト') }}
+                                    </button>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                         @csrf
