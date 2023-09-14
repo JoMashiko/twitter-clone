1cf649256c75c75ca3a0f829545a42be30741ccc
JoMashiko <j.mashiko@hajimari.inc>
2023-09-14 15:29:11 +0900

    Add: 'middleware' => 'auth'を追加

--- twitter/routes/web.php
+++ twitter/routes/web.php
@@ -23,29 +23,33 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-// マイページ
-Route::prefix('user/{id}')->group(function() {
-    // ユーザー詳細画面の表示
-    Route::get('/', [UserController::class, 'findByUserId'])->name('user.show');
-    // ユーザー編集画面の表示
-    Route::get('/edit', [UserController::class, 'edit'])->name('user.edit');
-    // ユーザー情報更新
-    Route::put('/update', [UserController::class, 'update'])->name('user.update');
-    // ユーザー削除
-    Route::delete('/', [UserController::class, 'delete'])->name('user.delete');
-});
+// 認証済み
+Route::group(['middleware' => 'auth'], function () {
 
-// ユーザー一覧
-Route::get('/users', [UserController::class, 'getAllUsers'])->name('user.index');
+    // マイページ
+    Route::prefix('user/{id}')->group(function() {
+        // ユーザー詳細画面の表示
+        Route::get('/', [UserController::class, 'findByUserId'])->name('user.show');
+        // ユーザー編集画面の表示
+        Route::get('/edit', [UserController::class, 'edit'])->name('user.edit');
+        // ユーザー情報更新
+        Route::put('/update', [UserController::class, 'update'])->name('user.update');
+        // ユーザー削除
+        Route::delete('/', [UserController::class, 'delete'])->name('user.delete');
+    });
 
-// ツイート
-Route::prefix('tweet')->group(function() {
-    // ツイート画面の表示
-    Route::get('/create', [TweetController:: class, 'create'])->name('tweet.create');
-    // ツイート保存
-    Route::post('/store', [TweetController:: class, 'store'])->name('tweet.store');
-    // ツイート詳細画面の表示
-    Route::get('/{id}', [TweetController:: class, 'findByTweetId'])->name('tweet.show');
+    // ユーザー一覧
+    Route::get('/users', [UserController::class, 'getAllUsers'])->name('user.index');
+
+    // ツイート
+    Route::prefix('tweet')->group(function() {
+        // ツイート画面の表示
+        Route::get('/create', [TweetController:: class, 'create'])->name('tweet.create');
+        // ツイート保存
+        Route::post('/store', [TweetController:: class, 'store'])->name('tweet.store');
+        // ツイート詳細画面の表示
+        Route::get('/{id}', [TweetController:: class, 'findByTweetId'])->name('tweet.show');
+    });
 });
 
 // ツイート一覧
