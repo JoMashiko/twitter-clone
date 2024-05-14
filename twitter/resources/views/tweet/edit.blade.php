92dfb9a6e270373862b31deca8b952369002f49c
JoMashiko <j.mashiko@hajimari.inc>
2024-05-14 21:33:26 +0900

    test

--- twitter/resources/views/tweet/edit.blade.php
+++ twitter/resources/views/tweet/edit.blade.php
@@ -1,32 +1,52 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('編集') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('編集') }}</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('tweet.update', $tweet) }}">
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('tweet.update', $tweet) }}">
 
-                        @csrf
-                        @method('put')
-                        <div class="mb-3">
-                            <textarea class="form-control @error('body') is-invalid @enderror" id="body" rows="3" type="text" name='body' value="{{ old('body') ?? $tweet->body }}" required autofocus></textarea>
-                            @error('body')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                            @enderror
-                        </div>
-                        <div class="d-grid justify-content-md-end">
-                            <button class="btn btn-primary" type="submit">{{ __('保存') }}</button>
-                        </div>
-                    </form>
+                            @csrf
+                            @method('put')
+                            <div class="mb-3">
+                                <textarea class="form-control @error('body') is-invalid @enderror" id="body" rows="3" type="text"
+                                    name='body' value="{{ old('body') ?? $tweet->body }}" required autofocus></textarea>
+                                @error('body')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                            <div class="d-grid justify-content-md-end">
+                                <button class="btn btn-primary" type="submit">{{ __('保存') }}</button>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('tweet.update', $tweet) }}">
+
+                            @csrf
+                            @method('put')
+                            <div class="mb-3">
+                                <textarea class="form-control @error('body') is-invalid @enderror" id="body" rows="3" type="text"
+                                    name='body' value="{{ old('body') ?? $tweet->body }}" required autofocus></textarea>
+                                @error('body')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                            <div class="d-grid justify-content-md-end">
+                                <button class="btn btn-primary" type="submit">{{ __('保存') }}</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
